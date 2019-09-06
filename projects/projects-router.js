56ac97a872cf87575e62459f715175d5ffa09ad4
Richard Lawson <lawson.a.richard@gmail.com>
2019-09-06 10:50:45 -0700

    can delete, get and put
    can delete, get and put project posts

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -2,16 +2,74 @@ const express = require("express");
 const router = express.Router();
 router.use(express.json());
 
-router.get("/", (req, res) => {});
+const actModel = require("../data/helpers/actionModel");
 
-router.get("/:id", (req, res) => {});
+router.get("/", (req, res) => {
+  actModel
+    .get()
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Cannot get projects" });
+    });
+});
 
-router.delete("/:id", (req, res) => {});
+router.get("/:id", validateId, (req, res) => {
+  const { id } = req.params;
+  actModel.get(id).then(project => {
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(401).json({ error: "Project with this ID does not exist" });
+    }
+  });
+});
 
-router.put("/:id", (req, res) => {});
+router.delete("/:id", validateId, (req, res) => {
+  const { id } = req.params;
+  actModel
+    .remove(id)
+    .then(() => res.status(204).end())
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error deleting Project" });
+    });
+});
+
+router.put("/:id", validateId, (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+  const { name } = req.body;
+  actModel
+    .update(id, { description }, { name })
+    .then(() => {
+      actModel
+        .get(id)
+        .then(project => res.status(200).json(project))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({ error: "Error getting Project" });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error updating Project" });
+    });
+});
 
 // custom middleware
 
-function validatePostId(req, res, next) {}
+function validateId(req, res, next) {
+  const { id } = req.params.id;
+  actModel.get(id).then(project => {
+    if (project) {
+      next();
+    } else {
+      res.status(404).json({ error: "Project with this id does not exist" });
+    }
+  });
+}
 
 module.exports = router;
