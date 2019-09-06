ccefe23bb7167a1055e85c3405742f82c3378ac8
Richard Lawson <lawson.a.richard@gmail.com>
2019-09-06 08:59:38 -0700

    Server is up
    server is up and running!

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+// Routers
+// const userRouter = require("./users/userRouter");
+// server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Hello Peeps</h2>`);
+});
+
+//custom middleware
+
+// function logger(req, res, next) {
+// console.log(req.method, req.url, Date.now());
+// next();
+// }
+
+module.exports = server;
